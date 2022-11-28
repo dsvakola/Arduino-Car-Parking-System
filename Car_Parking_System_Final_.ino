b45f59f1c57a60bef9b177f9115cdd65f583c2fa
Vidyasagar Academy Akola <35340141+dsvakola@users.noreply.github.com>
2022-11-28 09:19:52 +0530

    Add files via upload

---
+++ Car_Parking_System_Final_.ino
@@ -0,0 +1,46 @@
+
+
+  #include <Servo.h> 
+  
+  Servo i;
+  int EntrySensor=1;
+  int ExitSensor=2;
+  int flag1=0;
+  int flag2=0;
+
+  void setup() 
+  {
+    pinMode(EntrySensor,INPUT);
+    pinMode(ExitSensor,INPUT);
+    i.attach(11);
+    i.write(0); // gate is closed
+  }
+
+  void loop()
+  { 
+    if(digitalRead(EntrySensor)==LOW && flag1==0)
+    {
+      flag1=1;
+        if(flag2==0)
+        {
+          i.write(100); 
+        }
+    }
+  
+  if(digitalRead(ExitSensor)==LOW && flag2==0)
+  {
+     flag2=1;
+       if(flag1==0)
+       {
+         i.write(100); 
+       }
+    }
+  
+  if(flag1==1 && flag2==1)
+  {
+    delay (1000);
+    i.write(0);
+    delay(100);
+    flag1=0, flag2=0;
+  }
+  }
